dcb5277a43f95ed0d343789f58cc283d2dc78343
joaquinPega <joaquin_pega@hotmail.com>
2014-09-04 10:43:18 -0300

    Added modelo

---
+++ test/modelo/src/main/java/org/modelo/App.java
@@ -0,0 +1,13 @@
+package org.modelo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
